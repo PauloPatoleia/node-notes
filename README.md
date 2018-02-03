0022237db511ae78e1d68ab0b7fc7e28ef2e6440
FixTheInternet <vmpatoleia@protonmail.com>
2018-02-03 13:25:31 +0000

    Added functionality to the 'add' command

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node-notes
 Simple command line notes app
+
+# Commands
+
+add - Adds a note to the file, takes 2 arguments: --title and --body
+Example: node app.js add --title="Game idea" --body="A game about hacking that can be played by non-technical people"
--- notes.js
+++ notes.js
@@ -16,8 +16,15 @@ var addNote = (title, body) => {
 
   }
 
-  notes.push(note);
-  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+  duplicateNotes = notes.filter((note) => note.title === title)
+
+  if(duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+
+  } else {
+    console.log('Title already exists');
+  }
 
 };
 
